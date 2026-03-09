01d2b60d736eb2c2970aaa654323c79a63e5a63c
Kidoz203 <phone2003217@gmail.com>
2026-03-09 14:10:55 +0700

    delete build/

--- backend/build/lib/app/api/deps.py
+++
@@ -1,4 +0,0 @@
-# backend/app/api/deps.py
-from app.db.dependencies import get_db
-
-__all__ = ["get_db"]
--- backend/build/lib/app/repositories/__init__.py
+++
@@ -1,4 +0,0 @@
-# backend/app/repositories/__init__.py
-from app.repositories import physical_table_repo, table_group_repo
-
-__all__ = ["physical_table_repo", "table_group_repo"]
